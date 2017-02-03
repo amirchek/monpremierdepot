a0a37296d0ca9c7a43abf0044e2f3c8496ae6b7c
Patrick <Patrick@172.29.78.187>
2017-02-02 21:55:09 -0500

    nouveau

--- src/labgit/Labgit.java
+++ src/labgit/Labgit.java
@@ -16,7 +16,7 @@ public class Labgit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // voici mon projet ohhhhh yeahhhhhh 
+        // voici le nouveau commentaire 
         
     }
     
